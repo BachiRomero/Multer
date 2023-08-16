8227ab178819239f850bd3350cb6a12ae1740482
BachiRomero <romerobasilio@hotmail.com.ar>
2023-08-16 12:29:37 -0300

    Ejemplo de como subir un archivo con Multer

--- src/index.js
+++ src/index.js
@@ -4,38 +4,40 @@ import { dirname, extname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 const PORT = 3000;
+
+// sacar ruta relativa
 const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
-const MIMETYPES = ['image/jpeg', 'image/png'];
+const MIMETYPES = ['image/jpg', 'image/png', 'image/jpeg'];
 
 const multerUpload = multer({
+    // definimos carpeta de Destino
     storage: multer.diskStorage({
         destination: join(CURRENT_DIR, '../uploads'),
         filename: (req, file, cb) => {
             const fileExtension = extname(file.originalname);
             const fileName = file.originalname.split(fileExtension)[0];
-
             cb(null, `${fileName}-${Date.now()}${fileExtension}`);
         },
     }),
+    // filtramos tipos de archivos
     fileFilter: (req, file, cb) => {
         if (MIMETYPES.includes(file.mimetype)) cb(null, true);
         else cb(new Error(`Only ${MIMETYPES.join(' ')} mimetypes are allowed`));
     },
+    // definimos limites
     limits: {
+        //limite de tamaño
         fieldSize: 10000000,
     },
 });
 
-const expressApp = express();
+const server = express();
 
-expressApp.post('/upload', multerUpload.single('file'), (req, res) => {
+server.post('/upload', multerUpload.single('file'), (req, res) => {
     console.log(req.file);
-
     res.sendStatus(200);
 });
 
-expressApp.use('/public', express.static(join(CURRENT_DIR, '../uploads')));
-
-expressApp.listen(PORT, () => {
-    console.log(`Servidor levantado en el puerto ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server listening in port ${PORT}`);
 });
